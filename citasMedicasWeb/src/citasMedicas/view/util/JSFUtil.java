89171da3141799315af86bd4cc9fe4caf7d12d8c
alexandercastro <alexandercastro@10.0.2.15>
2020-09-20 01:26:41 -0500

    Implementación JSFUtil

---
+++ citasMedicasWeb/src/citasMedicas/view/util/JSFUtil.java
@@ -0,0 +1,36 @@
+package citasMedicas.view.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ * Clase JSF utilitaria, implementa varias funciones auxiliares 
+ * para la creación de mensajes personalizados que podran ser 
+ * visualizados en la capa view (páginas JSF).
+ * @author alexandercastro
+ *
+ */
+public class JSFUtil {
+	//Mensaje Informativo
+	public static void crearMensajeInfo(String mensaje) {
+		FacesMessage msg = new FacesMessage();
+		msg.setSeverity(FacesMessage.SEVERITY_INFO);
+		msg.setSummary(mensaje);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	//Mensaje de advertencia
+	public static void crearMensajeWarning(String mensaje) {
+		FacesMessage msg = new FacesMessage();
+		msg.setSeverity(FacesMessage.SEVERITY_WARN);
+		msg.setSummary(mensaje);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	//Mensaje de error
+	public static void crearMensajeError(String mensaje) {
+		FacesMessage msg = new FacesMessage();
+		msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+		msg.setSummary(mensaje);
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+	
+}
